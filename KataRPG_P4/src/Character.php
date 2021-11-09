6392e6e218331738b01b3224cad845f68c7c9ff0
MeliMano <melimanoni@gmail.com>
2021-11-09 09:09:43 +0100

    added kata

---
+++ KataRPG_P4/src/Character.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+class Character
+{
+    private int $health = 1000;
+    private int $level = 1;
+    private bool $alive = true;
+    
+    public function hit($damaged, Character $victim) {
+        $victimLifePoints = $victim->getHealth(); 
+        $victimLifePoints -= $damaged;
+        $victim-> setHealth($victimLifePoints);
+    }
+
+    public function setHealth(int $lifePoints, Character $character) {
+        $character->health = $lifePoints;
+    }
+    
+    public function getHealth()
+    {
+       return $this->health;
+    }
+
+   public function getLevel()
+   {
+       return $this->level;
+    }
+
+   public function isAlive()
+   {
+       return $this->alive;
+    }
+
+}
